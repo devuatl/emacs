231000197a8aeaa76e34467c55000e9af15f9598
Lukasz Kieda <lukasz.kieda@u2i.com>
2013-11-30 17:49:49 +0100

    Add auto-complete and ido-mode

--- Cask
+++ Cask
@@ -2,5 +2,7 @@
 (source gnu)
 (source marmalade)
 
+(depends-on "auto-complete")
 (depends-on "feature-mode")
+(depends-on "ido-vertical-mode")
 (depends-on "yaml-mode")
--- init.el
+++ init.el
@@ -2,4 +2,16 @@
 (cask-initialize)
 (require 'pallet)
 (add-to-list 'load-path "~/.emacs.d/custom")
+
+(ido-mode t)
+(require 'ido)
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories
+             "~/.emacs.d/.cask/24.3.50.1/elpa/auto-complete-20130724.1750/dict")
+(ac-config-default)
+(setq ac-ignore-case nil)
+(add-to-list 'ac-modes 'enh-ruby-mode)
+(add-to-list 'ac-modes 'web-mode)
+
 (load "00common-setup.el")
