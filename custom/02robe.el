659e38f02428ae784d0bdf345ae4a81c7b63bedc
Lukasz Kieda <lukasz.kieda@u2i.com>
2013-12-02 19:06:57 +0100

    Updated robe config

---
+++ custom/02robe.el
@@ -0,0 +1,8 @@
+(add-hook 'ruby-mode-hook 'robe-mode)
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+
+; - M-. to jump to the definition
+; - M-, to jump back
+; - C-c C-d to see the documentation
+; - C-c C-k to refresh Rails environment
+; - C-M-i to complete the symbol at point
